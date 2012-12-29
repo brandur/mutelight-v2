7712ee835113b67b56258f6028a038771dabbc00
Brandur <brandur@mutelight.org>
2012-12-29 11:51:45 -0800

    Update article title (the old one was terrible)

--- articles/caps-lock.rb
+++ articles/caps-lock.rb
@@ -1,4 +1,4 @@
-{ title:        "Caps Lock: What is it Good For?",
+{ title:        "Caps Lock + Tmux",
   slug:         "caps-lock",
   published_at: Time.parse("Sat Aug 19 08:43:46 PDT 2012"),
   location:     "San Francisco",
