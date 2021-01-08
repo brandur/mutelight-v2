07d13c971294d0ed30620fb4d4e9b8e52315a8f9
Brandur <brandur@brandur.org>
2021-01-07 23:14:09 -0700

    Fix nested quotes

--- articles/comments-on-leaving-dot-net.rb
+++ articles/comments-on-leaving-dot-net.rb
@@ -1,5 +1,5 @@
 {
-  title: "Comments on "Leaving .NET"",
+  title: "Comments on \"Leaving .NET\"",
   slug: "comments-on-leaving-dot-net",
   tiny_slug: "36",
   published_at: Time.parse("2010-12-16"),
